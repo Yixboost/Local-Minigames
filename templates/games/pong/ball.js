5a172a68abfd31841778bf78aef5a9a9be298ddd
valdtaniem <valdtaniem@gmail.com>
2024-11-18 20:07:42 +0100

    quick edit

--- templates/games/pong/ball.js
+++ templates/games/pong/ball.js
@@ -36,46 +36,6 @@ Ball.prototype.reset = function() {
   if (Math.random() > 0.5) this.xVelocity *= -1
 }
 
-function getCookie(name) {
-  // Zoek naar cookie
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return parts.pop().split(';').shift(); // Return waarde cookie
-  }
-  return null; // Cookie niet gevonden
-}
-
-function increaseScore(points) {
-  const nickname = getCookie("nickname");
-
-  fetch('/increase_point', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      nickname: nickname,
-      points: points
-    })
-  })
-  .then(response => {
-    console.log("Ruwe respons:", response); // Log de respons
-    return response.text(); // Eerst als tekst uitlezen
-  })
-  .then(text => {
-    console.log("Respons als tekst:", text); // Bekijk de respons
-    return JSON.parse(text); // Probeer het daarna te parsen
-  })
-  .then(data => {
-    alert(`Score van ${data.nickname} is nu: ${data.new_score}`);
-  })
-  .catch(error => {
-    alert(`Fout: ${error.message}`);
-    console.error("Details:", error);
-  });
-}
-
 Ball.prototype.update = function() {
   Entity.prototype.update.apply(this, arguments)
 
@@ -112,7 +72,6 @@ Ball.prototype.update = function() {
   // Off screen on right. Player wins.
   if (this.x > game.width) {
     game.player.score += 1
-    increaseScore(1)
     this.reset()
   }
 }
